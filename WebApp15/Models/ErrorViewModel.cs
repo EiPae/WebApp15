452f20b10c95562b47503d09128282b7bac4b96a
EiPae <eithaepomyanmar05033@gmail.com>
2024-06-19 22:36:43 +0900

    Upload  web App14

---
+++ WebApp15/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApp15.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
